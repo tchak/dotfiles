eaf6be55f44cfbdf2fd2d66903b5a85d156bcf62
tchak <paul@chavard.net>
2010-09-29 11:33:54 +0200

    gemrc and gitconfig

--- bash_aliases
+++ bash_aliases
@@ -9,6 +9,8 @@ mkdir2() { mkdir "$1" && cd "$1"; }
 
 alias  hi=history
 alias  hst=history
+alias  psgrep='ps aux | egrep -v egrep | egrep'
+alias  higrep='history | egrep -v egrep | egrep'
 alias  cp='cp -iv'
 alias  rm='rm -iv'
 alias  mv='mv -i'
@@ -16,3 +18,4 @@ alias  sln='ln -s'
 alias  cat='cat -v'
 alias  df='df -h'
 alias  du='du -h'
+alias  r='rails'
--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # Copy dot files
-cp ./bashrc ~/.bashrc
 cp ./bash_profile ~/.bash_profile
 cp ./bash_aliases ~/.bash_aliases
-source ~/.bashrc
+cp ./gemrc ~/.gemrc
+cp ./gitconfig ~/.gitconfig
+cp ./gitignore ~/.gitignore
+source ~/.bash_profile
+
+#mkdir ~/.bin
+#curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/.bin/webkit2png
+#chmod +x ~/.bin/webkit2png
